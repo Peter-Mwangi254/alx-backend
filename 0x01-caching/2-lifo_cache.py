461f158e600e790a7ff761099a0df4149ed229ce
Peter-Mwangi254 <pm6093628@gmail.com>
2024-06-26 14:48:01 -0700

    LIFO Caching

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,1 +1,36 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+class LIFOCache that inherits from
+BaseCaching and is a caching system
+"""
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """ LIFO cache system """
+
+    def __init__(self):
+        """ Initialize LIFO cache """
+        super().__init__()
+        self.stack = []
+
+    def put(self, key, item):
+        """ Add an item in the cache """
+        if key is None or item is None:
+            pass
+        else:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS \
+                    and key not in self.cache_data.keys():
+                last_key, last_value = self.cache_data.popitem()
+                print("DISCARD: {}". format(last_key))
+
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        return the item by key value
+        """
+        if key is None or key not in self.cache_data.keys():
+            return None
+        return self.cache_data.get(key)
