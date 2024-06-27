bf23c01a5b0186060e1ee1471040e11078431bbb
Peter-Mwangi254 <pm6093628@gmail.com>
2024-06-27 01:46:03 -0700

    MRU caching

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,1 +1,46 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+MRU Caching
+"""
+
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """
+    MRU cache system
+    """
+
+    def __init__(self):
+        """
+        Initialize MRU cache
+        """
+        super().__init__()
+        self.queue = []
+
+    def put(self, key, item):
+        """
+        Adds item in the cache
+        """
+        if key and item:
+            if key in self.cache_data:
+                self.cache_data[key] = item
+                self.queue.remove(key)
+            else:
+                if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                    discard = self.queue.pop()
+                    del self.cache_data[discard]
+                    print("DISCARD:", discard)
+                self.cache_data[key] = item
+            self.queue.append(key)
+
+    def get(self, key):
+        """
+        Get an item by key
+        """
+        if key in self.cache_data:
+            self.queue.remove(key)
+            self.queue.append(key)
+            return self.cache_data[key]
+        return None
