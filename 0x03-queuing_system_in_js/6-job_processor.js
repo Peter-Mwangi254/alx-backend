56cb41e0d0dc67153cb02eb839b16d8e739c988c
Peter-Mwangi254 <pm6093628@gmail.com>
2024-07-30 16:55:17 +0000

    Create the Job processor that works with job creator

--- 0x03-queuing_system_in_js/6-job_processor.js
+++ 0x03-queuing_system_in_js/6-job_processor.js
@@ -1,1 +1,14 @@
-#!/usr/bin/node
+const kue = require('kue');
+const queue = kue.createQueue();
+
+// Function to send notifications
+function sendNotification(phoneNumber, message) {
+    console.log(`Sending notification to ${phoneNumber}, with message: ${message}`);
+}
+
+// Queue process to listen to new jobs on push_notification_code
+queue.process('push_notification_code', (job, done) => {
+    const { phoneNumber, message } = job.data;
+    sendNotification(phoneNumber, message);
+    done();
+});
