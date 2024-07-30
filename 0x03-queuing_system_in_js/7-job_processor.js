6d5ac0275c68ca9ddf7eafbd84a99e3dfe7f4492
Peter-Mwangi254 <pm6093628@gmail.com>
2024-07-30 17:13:18 +0000

    Tracked progress and errors with Kue: Create the Job processor

--- 0x03-queuing_system_in_js/7-job_processor.js
+++ 0x03-queuing_system_in_js/7-job_processor.js
@@ -1,1 +1,26 @@
-#!/usr/bin/node
+const kue = require('kue');
+const queue = kue.createQueue();
+
+// Array of blacklisted phone numbers
+const blacklistedNumbers = ['4153518780', '4153518781'];
+
+// Function to send notifications
+function sendNotification(phoneNumber, message, job, done) {
+    job.progress(0, 100); // Track progress
+
+    if (blacklistedNumbers.includes(phoneNumber)) {
+        return done(new Error(`Phone number ${phoneNumber} is blacklisted`));
+    }
+
+    job.progress(50, 100); // Track progress
+
+    console.log(`Sending notification to ${phoneNumber}, with message: ${message}`);
+    done();
+}
+
+// Create a queue with Kue to process jobs with a concurrency of 2
+queue.process('push_notification_code_2', 2, (job, done) => {
+    const { phoneNumber, message } = job.data;
+    
+    sendNotification(phoneNumber, message, job, done);
+});
