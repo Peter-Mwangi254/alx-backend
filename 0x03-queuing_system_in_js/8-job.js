096c69489c377ff726a4fc798a562746a70902ec
Peter-Mwangi254 <pm6093628@gmail.com>
2024-07-30 17:30:07 +0000

    Writing the job creation function

--- 0x03-queuing_system_in_js/8-job.js
+++ 0x03-queuing_system_in_js/8-job.js
@@ -1,1 +1,35 @@
-#!/usr/bin/node
+const kue = require('kue');
+const queue = kue.createQueue();
+
+function createPushNotificationsJobs(jobs, queue) {
+    if (!Array.isArray(jobs)) {
+        throw new Error('Jobs is not an array');
+    }
+
+    jobs.forEach((jobData) => {
+        const job = queue.create('push_notification_code_3', jobData)
+            .save(function(err) {
+                if (!err) {
+                    console.log(`Notification job created: ${job.id}`);
+                }
+            });
+
+        // Listen for job completion
+        job.on('complete', function() {
+            console.log(`Notification job ${job.id} completed`);
+        });
+
+        // Listen for job failure
+        job.on('failed', function(err) {
+            console.log(`Notification job ${job.id} failed: ${err}`);
+        });
+
+        // Listen for job progress
+        job.on('progress', function(progress) {
+            console.log(`Notification job ${job.id} ${progress}% complete`);
+        });
+    });
+}
+
+
+module.exports = createPushNotificationsJobs;
